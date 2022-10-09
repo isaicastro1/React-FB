bf90ed7d64d66817f0242ae2dd7d16de25dd74d3
Isai Castro <isaicstr@gmail.com>
2022-10-08 17:16:04 -0700

    Updated post component. Added image, likes, and comment buttons

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -22,6 +22,40 @@ const Post = () => {
       <div className="post-text">
         Hello World Hello World Hello World Hello World
       </div>
+
+      <div className="post-image">
+        <img alt="" src="https://source.unsplash.com/featured/500x625" />
+      </div>
+
+      <div className="post-reactions">
+        <div className="post-likes">
+          <div className="icon-like">
+            <span className="material-symbols-outlined post-icon-like">
+              thumb_up
+            </span>
+          </div>
+          <div className="icon-heart">
+            <span className="material-symbols-outlined post-icon-heart">
+              favorite
+            </span>
+          </div>
+          <p className="friend-likes">Alessandra Ramos and 10 others</p>
+        </div>
+        <div className="post-like-comment">
+          <div className="post-like">
+            <span className="material-symbols-outlined post-icon">
+              thumb_up
+            </span>
+            <p className="post-like-text">Like</p>
+          </div>
+          <div className="post-comment">
+            <span className="material-symbols-outlined post-icon">
+              chat_bubble
+            </span>
+            <p className="post-comment-text">Comment</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
